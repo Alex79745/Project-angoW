23f6bd6e131f71a90dfcdeef08c451833ba723fa
Alex79745 <58765999+Alex79745@users.noreply.github.com>
2025-12-16 18:32:00 +0000

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,205 +1,62 @@
-locals {
-  primary_control_node_name = one(keys(var.control_nodes))
-  primary_control_node_ip   = var.control_node_ips[local.primary_control_node_name]
-
-  control_node_ips = values(var.control_node_ips)
-  worker_node_ips  = values(var.worker_node_ips)
-  node_ips         = concat(local.control_node_ips, local.worker_node_ips)
-}
-
-resource "proxmox_virtual_environment_download_file" "talos_image" {
-  content_type = "iso"
-  datastore_id = var.proxmox_iso_datastore
-  node_name    = values(var.control_nodes)[0]
-
-  url = "https://factory.talos.dev/image/${var.talos_schematic_id}/v${var.talos_version}/metal-${var.talos_arch}.qcow2"
-
-  file_name = "${var.talos_cluster_name}-talos-${var.talos_version}-${var.talos_arch}.img"
-}
-
-# ------------------------
-# CONTROL PLANE VMs
-# ------------------------
-resource "proxmox_virtual_environment_vm" "talos_control_vm" {
-  for_each  = var.control_nodes
-  name      = each.key
-  node_name = each.value
-
-  agent {
-    enabled = true
-  }
-
-  cpu {
-    cores = var.proxmox_control_vm_cores
-    type  = var.proxmox_vm_type
-  }
-
-  memory {
-    dedicated = var.proxmox_control_vm_memory
-    floating  = var.proxmox_control_vm_memory
-  }
-
-  disk {
-    datastore_id = var.proxmox_image_datastore
-    file_id      = proxmox_virtual_environment_download_file.talos_image.id
-    interface    = "virtio0"
-    size         = var.proxmox_control_vm_disk_size
-    discard      = "on"
-    iothread     = true
-  }
-
-  network_device {
-    bridge  = var.proxmox_network_bridge
-    vlan_id = var.proxmox_network_vlan_id
-  }
-
-  # ✅ NoCloud cloud-init (STATIC IP)
-  initialization {
-    interface = "scsi0"
-
-    ip_config {
-      ipv4 {
-        address = lookup(var.control_node_ips, each.key, null)
-        gateway = var.network_gateway
-      }
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "~> 0.75.0"
     }
-
-    #dns {
-    #  servers = var.dns_servers
-    #}
-  }
-
-  operating_system {
-    type = "l26"
-  }
-}
-
-# ------------------------
-# WORKER VMs
-# ------------------------
-resource "proxmox_virtual_environment_vm" "talos_worker_vm" {
-  for_each  = var.worker_nodes
-  name      = each.key
-  node_name = each.value
-
-  agent {
-    enabled = true
-  }
-
-  cpu {
-    cores = var.proxmox_worker_vm_cores
-    type  = var.proxmox_vm_type
-  }
-
-  memory {
-    dedicated = var.proxmox_worker_vm_memory
-    floating  = var.proxmox_worker_vm_memory
-  }
-
-  disk {
-    datastore_id = var.proxmox_image_datastore
-    file_id      = proxmox_virtual_environment_download_file.talos_image.id
-    interface    = "virtio0"
-    size         = var.proxmox_worker_vm_disk_size
-    discard      = "on"
-    iothread     = true
-  }
-
-  network_device {
-    bridge  = var.proxmox_network_bridge
-    vlan_id = var.proxmox_network_vlan_id
-  }
-
-  dynamic "disk" {
-    for_each = lookup(var.worker_extra_disks, each.key, [])
-    content {
-      datastore_id = disk.value.datastore_id
-      file_id      = disk.value.file_id
-      file_format  = disk.value.file_format
-      interface    = "virtio${disk.key + 1}"
-      size         = disk.value.size
-      discard      = "on"
-      iothread     = true
+    talos = {
+      source  = "siderolabs/talos"
+      version = "~> 0.7.1"
     }
   }
+}
 
-  # ✅ NoCloud cloud-init (STATIC IP)
-  initialization {
-    interface = "scsi0"
 
-    ip_config {
-      ipv4 {
-        address = lookup(var.worker_node_ips, each.key, null)
-        gateway = var.network_gateway
-      }
-    }
+provider "proxmox" {
+  endpoint = "https://192.168.1.129:8006/api2/json"
+  username = "root@pam"
+  password = "928195424"
+  insecure = true
+}
 
-    #dns {
-    #  servers = var.dns_servers
-    #}
+
+module "talos" {
+  source  = "./modules/terraform-proxmox-talos-main/"
+  #version = "0.1.5"
+
+  # name of the cluster
+  talos_cluster_name = "tf-test-cluster"
+  # choose Talos version you want
+  talos_version = "1.9.5"
+
+  # Create exactly 1 control and 1 worker (both on the same proxmox node "pve1")
+  control_nodes = {
+    "tf-control-0" = "KHABC"
+  }
+  worker_nodes = {
+    "tf-worker-0"  = "KHABC"
   }
 
-  operating_system {
-    type = "l26"
+   control_node_ips = {
+    tf-control-0 = "192.168.1.11/24"
   }
+
+  worker_node_ips = {
+    tf-worker-0 = "192.168.1.12/24"
+  }
+
+  network_gateway = "192.168.1.1"
+ 
 }
 
-# ------------------------
-# TALOS CONFIG
-# ------------------------
-resource "talos_machine_secrets" "talos_secrets" {}
-
-data "talos_machine_configuration" "control_mc" {
-  cluster_name     = var.talos_cluster_name
-  machine_type     = "controlplane"
-  cluster_endpoint = "https://${local.primary_control_node_ip}:6443"
-  machine_secrets  = talos_machine_secrets.talos_secrets.machine_secrets
+output "talos_config" {
+  description = "Talos configuration file (sensitive)"
+  value       = module.talos.talos_config
+  sensitive   = true
 }
 
-data "talos_machine_configuration" "worker_mc" {
-  cluster_name     = var.talos_cluster_name
-  machine_type     = "worker"
-  cluster_endpoint = "https://${local.primary_control_node_ip}:6443"
-  machine_secrets  = talos_machine_secrets.talos_secrets.machine_secrets
-}
-
-data "talos_client_configuration" "talos_client_config" {
-  cluster_name         = var.talos_cluster_name
-  client_configuration = talos_machine_secrets.talos_secrets.client_configuration
-  endpoints            = local.control_node_ips
-  nodes                = local.node_ips
-}
-
-resource "talos_machine_configuration_apply" "control_apply" {
-  for_each = var.control_nodes
-
-  client_configuration        = talos_machine_secrets.talos_secrets.client_configuration
-  machine_configuration_input = data.talos_machine_configuration.control_mc.machine_configuration
-  node                        = var.control_node_ips[each.key]
-  config_patches              = var.control_machine_config_patches
-
-  depends_on = [proxmox_virtual_environment_vm.talos_control_vm]
-}
-
-resource "talos_machine_configuration_apply" "worker_apply" {
-  for_each = var.worker_nodes
-
-  client_configuration        = talos_machine_secrets.talos_secrets.client_configuration
-  machine_configuration_input = data.talos_machine_configuration.worker_mc.machine_configuration
-  node                        = var.worker_node_ips[each.key]
-  config_patches              = var.worker_machine_config_patches
-
-  depends_on = [proxmox_virtual_environment_vm.talos_worker_vm]
-}
-
-resource "talos_machine_bootstrap" "bootstrap" {
-  node                 = local.primary_control_node_ip
-  client_configuration = talos_machine_secrets.talos_secrets.client_configuration
-}
-
-resource "talos_cluster_kubeconfig" "kubeconfig" {
-  depends_on = [talos_machine_bootstrap.bootstrap]
-
-  client_configuration = talos_machine_secrets.talos_secrets.client_configuration
-  node                 = local.primary_control_node_ip
+output "kubeconfig" {
+  description = "Kubeconfig file (sensitive)"
+  value       = module.talos.kubeconfig
+  sensitive   = true
 }
