13c4a25d44453d5ea80a86184a7f9450b1f9721d
Alex79745 <58765999+Alex79745@users.noreply.github.com>
2026-01-15 20:53:07 +0000

    Add Cilium BGP configuration and GatewayClass

--- text.ts
+++ text.ts
@@ -17,3 +17,104 @@ helm install cilium cilium/cilium --version 1.18.6 \
 https://docs.cilium.io/en/stable/installation/k8s-install-helm/
 https://docs.siderolabs.com/kubernetes-guides/cni/deploying-cilium
 https://docs.cilium.io/en/stable/helm-reference/
+
+##############################################################################################################################################
+
+##################################################################################
+
+apiVersion: cilium.io/v2alpha1
+kind: CiliumBGPAdvertisement
+metadata:
+  name: advertise-loadbalancers
+  labels:
+    advertise: "true" # Label used for selection
+spec:
+  # In 1.17, these must be in a list
+  advertisements:
+    - advertisementType: "Service"
+      service:
+        addresses:
+          - "LoadBalancerIP"
+      # This selector is optional; leave empty {} to advertise all LB services
+      selector:
+        matchLabels: {}
+#######################################################################################################################
+
+###############################################
+
+
+# 1. Update your existing ClusterConfig to use a PeerConfigRef
+apiVersion: cilium.io/v2alpha1
+kind: CiliumBGPClusterConfig
+metadata:
+  name: bgp-config
+spec:
+  nodeSelector:
+    matchLabels:
+      cilium-bgp: "enabled"
+  bgpInstances:
+    - name: "instance-64513"
+      localASN: 64513
+      peers:
+        - name: "proxmox-host"
+          peerASN: 64512
+          peerAddress: "192.168.1.138"
+          peerConfigRef:
+            name: "proxmox-peer-config" # Link to the new PeerConfig
+
+---
+# 2. Create the PeerConfig to hold the advertisement selector
+apiVersion: cilium.io/v2alpha1
+kind: CiliumBGPPeerConfig
+metadata:
+  name: "proxmox-peer-config"
+spec:
+  families:
+    - afi: ipv4
+      safi: unicast
+      advertisements:
+        matchLabels:
+          advertise: "true" # This selects the Advertisement resource below
+
+---
+# 3. Create the Advertisement with matching labels
+apiVersion: cilium.io/v2alpha1
+kind: CiliumBGPAdvertisement
+metadata:
+  name: "bgp-advertisements"
+  labels:
+    advertise: "true" # Matches the PeerConfig selector above
+spec:
+  advertisements:
+    - advertisementType: "Service"
+      service:
+        addresses:
+          - "LoadBalancerIP"
+###############################################
+
+apiVersion: gateway.networking.k8s.io/v1
+kind: GRPCRoute
+metadata:
+  name: consul-grpc-route
+  namespace: consul
+spec:
+  parentRefs:
+    - name: cilium-ingress # Points to your Cilium Gateway
+  rules:
+    - matches:
+        - method:
+            service: "consul.v1.ConfigEntryService" # Or relevant Consul gRPC service
+      backendRefs:
+        - name: consul-server
+          port: 8502
+
+###################################################
+
+# Save as gateway-class.yaml
+apiVersion: gateway.networking.k8s.io/v1
+kind: GatewayClass
+metadata:
+  name: envoy-gateway
+spec:
+  controllerName: gateway.envoyproxy.io/gatewayclass-controller
+
